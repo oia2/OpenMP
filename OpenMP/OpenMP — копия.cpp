8a56fd5a7c4e1e7236dedbdc916751e205d48771
Oleg Anisimov <oleg20027855@mail.ru>
2024-02-03 23:50:11 +0700

    OpenMP first and second task

---
+++ OpenMP/OpenMP — копия.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <vector>
+#include <chrono>
+#include <omp.h>
+
+using namespace std;
+
+void matrixThread(int size, __int64** Am, __int64** Bm, __int64** Rt)
+{
+#pragma omp parallel for
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            for (int k = 0; k < size; k++) {
+                Rt[i][j] += Am[i][k] * Bm[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    int size = 4096;
+
+    __int64** Am = new __int64* [size];
+    __int64** Bm = new __int64* [size];
+    __int64** Rs = new __int64* [size];
+    __int64** Rv = new __int64* [size];
+    __int64** Rt = new __int64* [size];
+    __int64** Rtv = new __int64* [size];
+
+    for (int i = 0; i < size; i++) {
+        Am[i] = new __int64[size];
+        Bm[i] = new __int64[size];
+        Rs[i] = new __int64[size];
+        Rv[i] = new __int64[size];
+        Rt[i] = new __int64[size];
+        Rtv[i] = new __int64[size];
+        for (int j = 0; j < size; j++) {
+            Am[i][j] = rand() % 10;
+            Bm[i][j] = rand() % 10;
+            Rs[i][j] = 0;
+            Rv[i][j] = 0;
+            Rt[i][j] = 0;
+            Rtv[i][j] = 0;
+        }
+    }
+
+
+    cout << "Scalar: \n";
+    auto start = chrono::system_clock::now();
+
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            for (int k = 0; k < size; ++k) {
+                Rs[i][j] += Am[i][k] * Bm[k][j];
+            }
+        }
+    }
+
+    auto end = chrono::system_clock::now();
+    chrono::duration<double> diff = end - start;
+    cout << "Time to calculate " << diff.count() << " s" << endl;
+
+    cout << "OpenMP: \n";
+
+    start = chrono::system_clock::now();
+    matrixThread(size, Am, Bm, Rt);
+    end = chrono::system_clock::now();
+    diff = end - start;
+    cout << "Time to calculate " << diff.count() << " s" << endl;
+
+
+    bool equal = true;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (Rs[i][j] != Rt[i][j]) {
+                equal = false;
+            }
+        }
+    }
+    if (equal) {
+        cout << "Matrix is equal\n";
+    }
+    else {
+        cout << "Matrix not equal\n";
+    }
+
+    for (int i = 0; i < size; i++) {
+        delete[] Am[i];
+        delete[] Bm[i];
+        delete[] Rs[i];
+        delete[] Rv[i];
+        delete[] Rt[i];
+        delete[] Rtv[i];
+    }
+
+    delete[] Am;
+    delete[] Bm;
+    delete[] Rs;
+    delete[] Rv;
+    delete[] Rt;
+    delete[] Rtv;
+
+    return 0;
+}
---
+++ OpenMP/OpenMP.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <omp.h>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        cv::Mat image = cv::imread("../giena1024x768.jpg");
+
+        setlocale(LC_ALL, "Russian");
+        if (image.empty()) {
+            std::cout << "Не удалось загрузить изображение." << std::endl;
+            return -1;
+        }
+
+        // Создание двух пустых Mat-объектов для модифицированных синего и желтого каналов
+        auto start = std::chrono::high_resolution_clock::now();
+        cv::Mat modifiedBlueChannel = cv::Mat::zeros(image.size(), CV_8U);
+        cv::Mat modifiedYellowChannel = cv::Mat::zeros(image.size(), CV_8U);
+
+#pragma omp parallel for
+        for (int y = 0; y < image.rows; y++) {
+            for (int x = 0; x < image.cols; x++) {
+                cv::Vec3b pixel = image.at<cv::Vec3b>(y, x);
+                uchar redv = pixel[2];
+                uchar greenv = pixel[1];
+                uchar bluev = pixel[0];
+
+                // Вычисление модифицированного синего канала
+                uchar Bv = bluev - (greenv + bluev) / 2;
+
+                // Вычисление модифицированного желтого канала
+                uchar Yv = redv + greenv - 2 * (std::abs(redv - greenv) + bluev);
+
+                // Запись результатов в соответствующие Mat-объекты
+                modifiedBlueChannel.at<uchar>(y, x) = Bv;
+                modifiedYellowChannel.at<uchar>(y, x) = Yv;
+            }
+        }
+
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        std::cout << i << ". Time " << diff.count() << " ms" << std::endl;
+        // Сохранение изображений в файлы
+        cv::imwrite("../modified_blue_channel.jpg", modifiedBlueChannel);
+        cv::imwrite("../modified_yellow_channel.jpg", modifiedYellowChannel);
+    }
+    return 0;
+}
